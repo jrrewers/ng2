aeb2707ce69785b9deb50845b6018f9c148f886d
Jarosław Rewers <jaroslaw.rewers@gmail.com>
2017-04-04 08:10:34 +0200

    in forms, after model driven forms

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -2,7 +2,9 @@ import {Component} from "@angular/core"
 import {AuthService} from "./auth.service"
 import {Router} from "@angular/router"
 @Component({
-    templateUrl: 'app/user/login.component.html'
+    templateUrl: 'app/user/login.component.html',
+    styles:[`
+    em {float: right; color: #e05c65; padding-left: 10px}`]
 })
 export class LoginComponent {
     constructor(private authService:AuthService, private router: Router){}
